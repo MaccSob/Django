4e8a94546c6e425ae25e4c2d53978a03f89dbf9e
MaccSob <71sobmaciej@gmail.com>
2026-03-28 23:49:47 +0100

    models

--- gamingblog/gamingapp/admin.py
+++ gamingblog/gamingapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from gamingapp.models import Post
+
+
+admin.site.register(Post)
+
 
 # Register your models here.
--- gamingblog/gamingapp/models.py
+++ gamingblog/gamingapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    created_at = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField (
+        blank = True,
+        null = True,
+        help_text="Post będzie widoczny na blogu od:"
+    )
+    def _str_(self):
+        return self.title
+    
