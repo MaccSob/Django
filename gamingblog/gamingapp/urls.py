e21a86410ab8e9cd73eb3d4f5e77f1ba3282e3c1
MaccSob <71sobmaciej@gmail.com>
2026-03-30 23:59:00 +0200

    urls, views

---
+++ gamingblog/gamingapp/urls.py
@@ -0,0 +1,2 @@
+from django.urls import path
+from gamingapp.views import BlogListView
--- gamingblog/gamingapp/views.py
+++ gamingblog/gamingapp/views.py
@@ -1,2 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from gamingapp.models import Post
 
+class BlogList(ListView):
+    model = Post
+    template = 'gamingapp/blog.html'
